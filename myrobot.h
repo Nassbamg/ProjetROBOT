7c2f3d7c2cd07c69087f510a398aa0f7eedf6a11
Nassbamg <65295458+Nassbamg@users.noreply.github.com>
2022-05-19 10:38:36 +0200

    Create myrobot.h

---
+++ myrobot.h
@@ -0,0 +1,38 @@
+#ifndef MYROBOT_H
+#define MYROBOT_H
+
+#include <QObject>
+#include <QTcpSocket>
+#include <QAbstractSocket>
+#include <QDebug>
+#include <QTimer>
+#include <QMutex>
+
+
+class MyRobot : public QObject {
+    Q_OBJECT
+public:
+    explicit MyRobot(QObject *parent = 0);
+    void doConnect();
+    void disConnect();
+    QByteArray DataToSend;
+    QByteArray DataReceived;
+    QMutex Mutex;
+    void SetRobot1(short speed1,short speed2,unsigned char SpeedFlag);
+    short CRC16(unsigned char *Adresse_tab , unsigned char Taille_max);
+
+signals:
+    void updateUI(const QByteArray Data);
+public slots:
+    void connected();
+    void disconnected();
+    void bytesWritten(qint64 bytes);
+    void readyRead();
+    void MyTimerSlot();
+
+private:
+    QTcpSocket *socket;
+    QTimer *TimerEnvoi;
+};
+
+#endif // MYROBOT_H
