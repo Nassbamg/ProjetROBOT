c202f0f8326e8febfd9de103f44e4160275d1b96
Nassbamg <65295458+Nassbamg@users.noreply.github.com>
2022-05-25 08:40:21 +0200

    Add files via upload
    Robot mouvements toutes directions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,16 +17,61 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_BUT1_pressed()
+//Bouton Avancer
+void MainWindow::on_ButUp_pressed()
 {
-    ui->label->setText("1");
+
     robot.SetRobot1(170,170,80);
 }
 
 
-void MainWindow::on_BUT1_released()
+void MainWindow::on_ButUp_released()
 {
-    ui->label->setText("0");
+
     robot.SetRobot1(0,0,80);
 }
+
+//Bouton Droite
+void MainWindow::on_ButRight_pressed()
+{
+
+    robot.SetRobot1(170,15,80);
+}
+
+
+void MainWindow::on_ButRight_released()
+{
+
+    robot.SetRobot1(0,0,80);
+}
+
+//Bouton Reculer
+void MainWindow::on_ButDown_pressed()
+{
+
+    robot.SetRobot1(170,170,0);
+}
+
+
+void MainWindow::on_ButDown_released()
+{
+
+    robot.SetRobot1(0,0,0);
+}
+
+//Bouton Gauche
+void MainWindow::on_ButLeft_pressed()
+{
+
+    robot.SetRobot1(15,170,80);
+}
+
+
+void MainWindow::on_ButLeft_released()
+{
+
+    robot.SetRobot1(0,0,80);
+}
+
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -21,9 +21,14 @@ public:
 
 
 private slots:
-    void on_BUT1_pressed();
-
-    void on_BUT1_released();
+    void on_ButUp_pressed();
+    void on_ButUp_released();
+    void on_ButRight_pressed();
+    void on_ButRight_released();
+    void on_ButDown_pressed();
+    void on_ButDown_released();
+    void on_ButLeft_pressed();
+    void on_ButLeft_released();
 
 private:
     Ui::MainWindow *ui;
